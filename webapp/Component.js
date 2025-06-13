41440fa71e206dfc6fe4d7e27d6fb5581d1f331f
Ho Thach Nguyen <ho.thach.nguyen@sap.com>
2025-06-13 19:00:48 +0500

    init

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+    return AppComponent.extend("gd.md.alpproject.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
